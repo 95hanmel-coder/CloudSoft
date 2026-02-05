9eaff261c2c58ea7ebe9628eb4dca8cc0e01e990
Hanita Melin <hanita.2010@live.se>
2026-02-05 13:21:37 +0100

    newsletter

---
+++ Controllers/NewsletterController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudSoft.Controllers;
+
+public class NewsletterController : Controller
+{
+    // GET: /Newsletter/Subscribe
+    public IActionResult Subscribe()
+    {
+        return View();
+    }
+
+    // POST: /Newsletter/Subscribe
+    [HttpPost]
+    public IActionResult Subscribe(string name, string email)
+    {
+        // Write to the console
+        Console.WriteLine($"New subscription - Name: {name} Email: {email}");
+
+        // Send a message to the user
+        return Content($"Thank you {name} for subscribing to our newsletter!");
+    }
+}
